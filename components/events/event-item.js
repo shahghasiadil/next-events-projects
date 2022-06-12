4d048f79bbe22932eeee7fd05bf16309828b0324
shahghasiadil <shaghasy555@gmail.com>
2022-06-12 22:24:42 +0430

    Button component added

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import classes from './event-item.module.css'
 import Button from "../ui/button";
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 function EventItem(props){
     const {title, image, date, location, id} = props
     const humanReadable = new Date(date).toLocaleDateString('en-US', {
@@ -16,17 +19,23 @@ function EventItem(props){
         <div className={classes.contents}>
             <div className={classes.summary}>
                   <h1>{title}</h1>
+                 
                   <div className={classes.date}>
+                  <DateIcon/>
                       <time>{humanReadable}</time>
                  </div>
             </div>
             <div className={classes.address}>
+                <AddressIcon/>
                 <address>
                     {formattedAdress}
                 </address>
             </div>
             <div className={classes.actions}>
-            <Button href={exploreLink}> Explore Event</Button>
+            <Button link={exploreLink}> 
+            <span>Explore Event</span>
+            <span className={classes.icon}><ArrowRightIcon/></span>
+            </Button>
             </div>
         </div>
     </li>
--- components/ui/button.js
+++ components/ui/button.js
@@ -2,7 +2,5 @@ import Link from "next/link"
 
 import classes from './button.module.css';
 export default function Button(props){
-    return <Link href={props.link}>
-       <a className={classes.btn}>{props.children}</a>  
-        </Link>
+    return (<Link href={props.link}><a className={classes.btn}>{props.children}</a></Link>);
 }
